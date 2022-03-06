1111a70529435515ebff7a7b66b4590a8a62cbd8
n00bNn00b <realdewan9@gmail.com>
2022-03-06 16:53:29 +0600

    bug fixed of unnecessary location

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 const API_KEY = `a515a1942b304b8ebd4231129222302`;
 
 // empty error
-const empyErr = () => {
+const emptyErr = () => {
   document.getElementById("empty-error").style.display = "block";
+  document.getElementById("current-data").style.display = "none";
 };
 // no location error
-
+const locationError = () => {
+  document.getElementById("location-error").style.display = "block";
+  document.getElementById("location-error").style.display = "none";
+  document.getElementById("current-data").style.display = "none";
+};
 // hide error
 const hideErr = () => {
   document.getElementById("current-data").style.display = "block";
@@ -16,7 +21,7 @@ const hideErr = () => {
 const searchTemparature = () => {
   const cityName = document.getElementById("city-name").value;
   if (cityName.length === 0) {
-    empyErr();
+    emptyErr();
   } else {
     const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=5&aqi=yes&alerts=yes`;
     fetch(url)
@@ -36,24 +41,33 @@ const setInnerText = (id, text) => {
 // display data
 
 const displayData = (temparature) => {
-  console.log(temparature);
-  setInnerText("city", temparature.location.name);
+  // console.log(temparature);
 
-  setInnerText("country", temparature.location.country);
-  setInnerText("timezone", temparature.location.tz_id);
-  setInnerText("localtime", temparature.location.localtime);
-  setInnerText("current-temp", temparature.current.temp_c);
-  setInnerText("feels-like", temparature.current.feelslike_c);
-  setInnerText("last-update", temparature.current.last_updated);
-  setInnerText("wind-speed", temparature.current.wind_kph);
-  setInnerText("wind-speedmiles", temparature.current.wind_mph);
-  setInnerText("wind-dir", temparature.current.wind_dir);
-  setInnerText("condition", temparature.current.condition.text);
+  try {
+    setInnerText("city", temparature.location.name);
+    setInnerText("country", temparature.location.country);
+    setInnerText("timezone", temparature.location.tz_id);
+    setInnerText("localtime", temparature.location.localtime);
+    setInnerText("current-temp", temparature.current.temp_c);
+    setInnerText("feels-like", temparature.current.feelslike_c);
+    setInnerText("last-update", temparature.current.last_updated);
+    setInnerText("wind-speed", temparature.current.wind_kph);
+    setInnerText("wind-speedmiles", temparature.current.wind_mph);
+    setInnerText("wind-dir", temparature.current.wind_dir);
+    setInnerText("condition", temparature.current.condition.text);
 
-  //   set weather icon
-  const getWeatherIcon = temparature.current.condition.icon;
-  const http = "http:";
-  const setWeatherIconUrl = http.concat(getWeatherIcon);
-  const weatherIcon = document.getElementById("weather-icon");
-  weatherIcon.setAttribute("src", setWeatherIconUrl);
+    //   set weather icon
+    const getWeatherIcon = temparature.current.condition.icon;
+    const http = "http:";
+    const setWeatherIconUrl = http.concat(getWeatherIcon);
+    const weatherIcon = document.getElementById("weather-icon");
+    weatherIcon.setAttribute("src", setWeatherIconUrl);
+    hideErr();
+  } catch (error) {
+    setInnerText(
+      "city",
+      temparature?.location?.name ? temparature.location.name : emptyErr()
+    );
+    // console.log(error);
+  }
 };
